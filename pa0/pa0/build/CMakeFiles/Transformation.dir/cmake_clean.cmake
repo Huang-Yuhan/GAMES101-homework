8d11107cc5ed67ead23f950684cac58a8a0d10d9
Huang_Yuhan <huang_yuhan@sjtu.edu.cn>
2023-04-29 10:22:40 +0800

    hw0

---
+++ pa0/pa0/build/CMakeFiles/Transformation.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Transformation.dir/main.o"
+  "CMakeFiles/Transformation.dir/main.o.d"
+  "Transformation"
+  "Transformation.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Transformation.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ pa0/pa0/solution.md
@@ -0,0 +1,21 @@
+# solution
+
+$P$的坐标为$(2,1)$,转换为齐次坐标为$(2,1,1)$
+
+旋转变换矩阵为
+$$
+\begin{bmatrix}
+\cos \frac{\pi}{4} & -\sin \frac{\pi}{4} & 0\\
+\sin \frac{\pi}{4} & \cos \frac{\pi}{4} & 0\\
+0 & 0 & 1
+\end{bmatrix}
+$$
+
+平移矩阵为
+$$
+\begin{bmatrix}
+1 & 0 & 1 \\
+0 & 1 & 2 \\
+0 & 0 & 1 \\
+\end{bmatrix}
+$$
